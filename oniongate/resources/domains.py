e07ff674f8e7dd2735295bcd89945a2c83632645
Donncha O'Cearbhaill <donncha@donncha.is>
2016-08-25 00:01:07 +0200

    Include DNS records in individual domain output

--- oniongate/resources/domains.py
+++ oniongate/resources/domains.py
@@ -4,12 +4,13 @@ Domain resource presented on the API interface
 import datetime
 from collections import OrderedDict
 
-from flask_restful import fields, inputs, marshal_with, reqparse, Resource, abort
+from flask_restful import fields, inputs, marshal_with, marshal, reqparse, Resource, abort
 from sqlalchemy import exc
 
 from .. import validators
 from ..utils import auth_domain
 from ..models import Domain
+from .records import record_fields
 
 
 new_domain_parser = reqparse.RequestParser()
@@ -37,6 +38,10 @@ domain_fields = OrderedDict([
     ('service_online', fields.Boolean)
 ])
 
+# Include DNS records when requesting an individual domain
+domain_fields_with_records = domain_fields.copy()
+domain_fields_with_records['records'] = fields.Nested(record_fields)
+
 # Return a serialisation with a JWT (JSON Web Token) for authenticating
 # future updates to this domain
 domain_fields_with_token = domain_fields.copy()
@@ -44,14 +49,16 @@ domain_fields_with_token['update_token'] = fields.String(attribute='token')
 
 
 class Domains(Resource):
-    @marshal_with(domain_fields)
     def get(self, domain_name=None):
         """
         Return a listing of all public domains resolved by this resolver
         """
         if domain_name:
-            return Domain.get_or_404(domain_name)
-        return Domain.query.filter_by(public=True, deleted=False).all()
+            # Marshal the individual domain with its DNS records
+            return marshal(Domain.get_or_404(domain_name), domain_fields_with_records), 200
+
+        all_domains = Domain.query.filter_by(public=True, deleted=False).all()
+        return marshal(all_domains, domain_fields), 200
 
     @marshal_with(domain_fields_with_token)
     def post(self):
